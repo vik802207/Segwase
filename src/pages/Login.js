e4f94b4c37b9b0d97cb0d1398143cd63fff02670
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-19 12:25:31 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8000/api/auth/login", {
+      const res = await axios.post("https://segwase.onrender.com/api/auth/login", {
         email,
         password,
       });
